8fa491507562cebc48b531e8eb1099ef2de05250
molo06002 <molo06002@E-PW0234KQ>
2023-10-04 10:46:03 +0200

    update 2.0

--- Inlämningsuppgift3.4/Program.cs
+++ Inlämningsuppgift3.4/Program.cs
@@ -8,17 +8,24 @@ namespace inlämningsuppgift3_4
             Console.WriteLine("Skriv in antal minuter låten är: ");
             string x = Console.ReadLine();
             int minuter = Convert.ToInt32(x);
+
             Console.WriteLine("Skriv in antal sekunder låten är: ");
             string y = Console.ReadLine();
             int sekunder = Convert.ToInt32(y);
+
+            int stor = (4*60)+20;
+            int liten = (2*60)+45;
+
+            int minuter1 = (minuter * 60) + sekunder;
             Console.Clear();
-            if (minuter >= 4 && sekunder >= 20)
+
+            if (minuter1 >= liten && minuter1 <= stor)
             {
-                Console.WriteLine("Låten är för stor och därav är inte kvalificerad");
+                Console.WriteLine("Låten är kvalificerad");
             }
             else
             {
-                Console.WriteLine("Låten är kvalificerad");//
+                Console.WriteLine("Låten är antingen för stor eller för liten");//
             }
         }
     }
